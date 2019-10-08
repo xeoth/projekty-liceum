bd284372ed4dc2879e9ee8e927b1ddb5a999db51
Xeoth <ujan.pl@hotmail.com>
2019-10-08 23:52:09 +0200

    Poprawiono P07

--- zadania-p/P07.cpp
+++ zadania-p/P07.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 int main() {
-	int liczba, silnia = 1;
+	//deklaracja zmiennych
+	int liczba;
+	unsigned long long silnia = 1; //unsigned long long bo potrzeba przechować wartość większą niż int
 	
+	//odbieranie wartości o użytkownika
 	do {
 		cout << "Podaj liczbe nie wieksza niz 20" << endl;
 		cin >> liczba;
 	} while (liczba > 20);
 	
+	//obliczanie silni
 	for (int i = 1; i <= liczba; i++) {
 		silnia*=i;
 	}
 	
+	//wyświetlanie wyniku
 	cout << liczba << "! = " << silnia;
 	
 	return 0;
