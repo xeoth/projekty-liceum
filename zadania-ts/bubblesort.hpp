3fe4d4779c2f633c677214e915b0bdd27ac352b0
Xeoth <ujan.pl@gmail.com>
2020-01-13 10:08:19 +0100

    Algorytm sortowania bąbelkowego

---
+++ zadania-ts/bubblesort.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int* bubbleSort(int* array, int len) {
+  for (int i = 0; i < len; i++)
+  {
+    for (int j = 0; j < len; j++)
+    {
+      if (array[j] > array[j+1]) {
+        int tempArray = array[j];
+        array[j] = array[j+1];
+        array[j+1] = tempArray;
+      }
+    }
+  }
+  return array;
+}
