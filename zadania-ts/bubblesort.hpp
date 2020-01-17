6640d32eb59d9eda722202559708c1593b4db13c
Xeoth <ujan.pl@gmail.com>
2020-01-17 09:55:24 +0100

    Fixed falling bubble sort

--- zadania-ts/bubblesort.hpp
+++ zadania-ts/bubblesort.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int* bubbleSortRising(int* array, int len) {
+int* bubbleSortRising(int* array, const int len) {
   for (int i = 0; i < len; i++)
   {
     for (int j = 0; j < len; j++)
@@ -16,19 +16,19 @@ int* bubbleSortRising(int* array, int len) {
   return array;
 }
 
-
-//FIXME: Returns error
-int* bubbleSortFalling(int* array, int len) {
-  int sortedRising = bubbleSortRising(array, len);
-  // Odwracanie tabeli
-  int counter = 0, sortedFalling[len];
-  for (int k = 8; k > 0; k--)
+int* bubbleSortFalling(int* array, const int len) {
+  int tempArray;
+  for (int i = 0; i < len; i++)
   {
-    sortedFalling[counter] = sortedRising[k];
-    cout << sortedFalling[counter] << ' ';
-    counter++;
+    for (int j = 0; j < len; j++)
+    {
+      if (array[j] < array[j+1]) {
+        int tempArray = array[j];
+        array[j] = array[j+1];
+        array[j+1] = tempArray;
+      }
+    }
   }
-
   return array;
   
 }
