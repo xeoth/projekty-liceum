917670863ed12a90503d58c2346ce8946104db0c
Xeoth <ujan.pl@gmail.com>
2020-02-26 19:50:00 +0100

    Dodałem cleanup

---
+++ cleanup.py
@@ -0,0 +1,22 @@
+"""
+Skrypt służący do usuwania wszystkich pozostałości po kompilacji (.exe, .out)
+"""
+
+import os
+
+main_dir = '../projekty-liceum'  # katalog główny
+unwanted_extensions = ('.exe', '.out')  # rozszerzenia plików do usunięcia
+
+
+def remove_all_in_dir(path):
+    for current_name in os.listdir(path):
+        full_path = os.path.join(path, current_name)
+        if os.path.isdir(full_path):
+            print(f'Entering: {full_path}')
+            remove_all_in_dir(f'{full_path}')
+        elif current_name.endswith(unwanted_extensions):
+            print(f'Removed: {full_path}')
+            os.remove(full_path)
+
+
+remove_all_in_dir(main_dir)
