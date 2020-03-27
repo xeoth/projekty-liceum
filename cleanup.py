9c877851277e9e368c1432cb6d55038b40565e6f
Xeoth <ujan.pl@gmail.com>
2020-03-28 00:10:16 +0100

    Wyrzuciłem bezpotrzebny string

--- cleanup.py
+++ cleanup.py
@@ -6,7 +6,7 @@ import os
 
 main_dir = '../projekty-liceum'  # katalog główny
 unwanted_extensions = ('.exe', '.out')  # rozszerzenia plików do usunięcia
-ignored_directories = ('../projekty-liceum/.git')  # ścieżki do katalogów, które mają być ignorowane
+ignored_directories = ('../projekty-liceum/.git', '../projekty-liceum/.vscode')  # ścieżki do katalogów, które mają być ignorowane
 
 
 def remove_all_in_dir(path):
@@ -14,7 +14,7 @@ def remove_all_in_dir(path):
         full_path = os.path.join(path, current_name)
         if os.path.isdir(full_path) and full_path not in ignored_directories:
             print(f'Entering: {full_path}')
-            remove_all_in_dir(f'{full_path}')
+            remove_all_in_dir(full_path)
         elif current_name.endswith(unwanted_extensions):
             print(f'Removed: {full_path}')
             os.remove(full_path)
