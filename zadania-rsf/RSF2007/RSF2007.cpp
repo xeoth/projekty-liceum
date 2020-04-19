a634c70f0c9c7951e154e4fe118d4fc19c03be9d
Xeoth <ujan.pl@gmail.com>
2020-04-19 21:37:43 +0200

    RSF2007.a działa; idk czy 2 jest pierwsza czy nie

---
+++ zadania-rsf/RSF2007/RSF2007.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cmath>
+#include <bitset>
+#include <fstream>
+using namespace std;
+
+// TODO: determine whether 2 is prime or not
+bool isPrime(long num)
+{
+    if (num == 2 || num == 3)
+        return true;
+    else if (num % 2 == 0 || num % 3 == 0)
+        return false;
+
+    int i = 5, w = 2;
+
+    while (i * i <= num)
+    {
+        if (num % i == 0)
+            return false;
+
+        i += w;
+        w = 6 - w;
+    }
+}
+
+int digitSum(long num)
+{
+    int sum = 0;
+    while (num >= 1)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+bool isSuperPrime(int num)
+{
+    if (isPrime(num) && isPrime(digitSum(num)))
+        return true;
+    else
+        return false;
+}
+
+bool isSuperBPrime(int num)
+{
+    string bit = bitset<64>(num).to_string();
+
+    if (isSuperPrime(num) && isPrime(digitSum(stol(bit))))
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    fstream t1, t2, t3;
+    t1.open("1.txt", ios::out);
+    t2.open("2.txt", ios::out);
+    t3.open("3.txt", ios::out);
+
+    // ppkt. a
+    {
+        // liczenie każdego przedziału
+        int countFirst = 0, countSecond = 0, countThird = 0;
+
+        // <2, 1000>
+        for (int i = 2; i <= 1000; i++)
+        {
+            if (isSuperBPrime(i))
+            {
+                countFirst++;
+                t1 << i << '\n';
+            }
+        }
+
+        // <100, 10000>
+        for (int j = 100; j <= 10000; j++)
+        {
+            if (isSuperBPrime(j))
+            {
+                countSecond++;
+                t2 << j << '\n';
+            }
+        }
+
+        // <1000, 100000>
+        for (int k = 1000; k <= 100000; k++)
+        {
+            if (isSuperBPrime(k))
+            {
+                countThird++;
+                t3 << k << '\n';
+            }
+        }
+
+        cout << "Nr. |    Przedzial    | L. wyst\n"
+             << "----+-----------------+--------\n"
+             << "1   |    <2,1000>     | " << countFirst << '\n'
+             << "2   |   <100,10000>   | " << countSecond << '\n'
+             << "3   |  <1000,100000>  | " << countThird << '\n';
+    }
+
+    return 0;
+}
