483e312a60cdc090507e44e058f8c0ab1c53479f
Xeoth <ujan.pl@gmail.com>
2020-04-22 16:00:55 +0200

    Wykonano RSF2008

--- zadania-rsf/RSF2008/RSF2008.cpp
+++ zadania-rsf/RSF2008/RSF2008.cpp
@@ -4,6 +4,26 @@
 #include <algorithm>
 using namespace std;
 
+string end(string word)
+{
+    string end, part;
+
+    for (int i = 0; i < word.size(); i++)
+    {
+        part += word[i];
+
+        string reversedPart = part;
+        reverse(reversedPart.begin(), reversedPart.end());
+
+        if (part == reversedPart)
+            end = "";
+        else
+            end = word[i] + end;
+    }
+
+    return end;
+}
+
 int main()
 {
 
@@ -36,5 +56,53 @@ int main()
                    << "Długość tego hasła to " << shortest.size() << ".";
     }
 
+    // ppkt. b
+    {
+        fstream read, writeSlowa, writeHasla;
+        read.open("slowa.txt", ios::in);
+        writeSlowa.open("slowa_b.txt", ios::out);
+        writeHasla.open("hasla_b.txt", ios::out);
+
+        writeSlowa << "Hasła mające 12 znaków:" << '\n';
+
+        string longest, shortest;
+        int lengthSum = 0;
+        for (int i = 0; i < 1000; i++)
+        {
+            string current;
+            read >> current;
+
+            string reversed = current;
+            reverse(reversed.begin(), reversed.end());
+
+            string password;
+            if (current == reversed)
+                password = current;
+            else
+                password = end(current) + current;
+
+            writeHasla << password << '\n';
+
+            // obliczanie najdłuższego hasła
+            if (!i) // i = 0
+                longest = shortest = password;
+            else
+            {
+                if (password.size() > longest.size())
+                    longest = password;
+                else if (password.size() < shortest.size())
+                    shortest = password;
+            }
+
+            if (password.size() == 12) // po co im hasła co mają akurat 12? :P
+                writeSlowa << password << '\n';
+
+            lengthSum += password.size();
+        }
+
+        writeSlowa << "\nNajdłuższe hasło: " << longest << '\n'
+                   << "Najkrótsze hasło: " << shortest << '\n'
+                   << "Suma długości: " << lengthSum;
+    }
     return 0;
 }
