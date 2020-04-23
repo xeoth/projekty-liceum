ecfa4c9f2310fea1f69c5074e37b0296afce881f
Xeoth <ujan.pl@gmail.com>
2020-04-23 15:16:25 +0200

    progress

--- zadania-rsf/RSF2010/RSF2010.cpp
+++ zadania-rsf/RSF2010/RSF2010.cpp
@@ -2,13 +2,27 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-bool areAllSizesEqual(vector<string> &line, int firstSize)
+bool isAnagram(string firstWord, string wordToCheck)
+{
+    vector<char> firstWordVector(firstWord.begin(), firstWord.end());
+
+    for (int i = 0; i < wordToCheck.size(); i++)
+    {
+        if (find(firstWord.begin(), firstWord.end(), wordToCheck[i]) == firstWord.end())
+            return false;
+    }
+
+    return true;
+}
+
+bool areAllSizesEqual(vector<string> &line)
 {
     for (int i = 1; i < 5; i++)
     {
-        if (line[i].size() != firstSize)
+        if (line[i].size() != line[0].size())
             return false;
     }
 
@@ -20,10 +34,9 @@ int main()
 
     // ppkt. a
     {
-        fstream read, write4a, write4b;
+        fstream read, write4a;
         read.open("anagram.txt", ios::in);
         write4a.open("odp_4a.txt", ios::out);
-        write4b.open("odp_4b.txt", ios::out);
 
         for (int i = 0; i < 200; i++)
         {
@@ -37,12 +50,38 @@ int main()
             }
 
             // porównywanie długości
-            if (areAllSizesEqual(line, line[0].size()))
+            if (areAllSizesEqual(line))
             {
                 for (string current : line)
                     write4a << current << ' ';
                 write4a << '\n';
             }
+        }
+    }
+
+    // ppkt. b
+    {
+        fstream read, write4b;
+        read.open("anagram.txt", ios::in);
+        write4b.open("odp_4b.txt", ios::out);
+
+        for (int i = 0; i < 200; i++)
+        {
+            vector<string> line;
+            for (int j = 0; j < 5; j++)
+            {
+                string current;
+                read >> current;
+                line.push_back(current);
+            }
+
+            for (int i = 1; i < line.size(); i++)
+                if (isAnagram(line[i], line[0]) && areAllSizesEqual(line))
+                {
+                    for (int k = 0; k < 5; k++)
+                        write4b << line[k] << ' ';
+                    write4b << '\n';
+                }
         }
     }
     return 0;
