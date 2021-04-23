2f1abedf46ef602844bdeb1881c95abb8433ba70
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-04-23 13:30:24 +0200

    Wykonano zadanie INF2021.2

---
+++ zadania-inf/INF2021/INF2021.2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int calculateBase(string number);
+int calculateSum(string number);
+
+int main() {
+    fstream read, write;
+    read.open("dane6.txt", ios::in);
+    write.open("wyniki6_2.txt", ios::out);
+
+    string bases[10]{};
+    int sums[10]{};
+
+    for (int i = 0; i < 2024; i++)
+    {
+        string current;
+        read >> current;
+
+        int base = calculateBase(current);
+        int sum = calculateSum(current);
+
+        if (sums[base] < sum)
+        {
+            sums[base] = sum;
+            bases[base] = current;
+        }
+    }
+
+    for (int ii = 1; ii < 10; ii++)
+    {
+        write << ii+1 << ": " << bases[ii] << '\n';
+    }
+	return 0;
+}
+
+int calculateBase(string number)
+{
+    int base = 0;
+    for (auto&& i : number)
+    {
+        int intLetter = i - '0';
+        if (intLetter > base)
+            base = intLetter;
+    }
+
+    return base;
+}
+
+int calculateSum(string number) {
+    int sum = 0;
+    for (char i : number)
+        sum += i - '0';
+    return sum;
+}
